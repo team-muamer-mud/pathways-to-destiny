40a76740939b14b75be1eadb09073d86b26486b1
Steve Renner <che1111@hotmail.com>
2020-03-03 08:42:36 -0800

    Changed gunicorn

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -151,7 +152,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 import django_heroku
 django_heroku.settings(locals())
