2a3e34ff936069ce8fdaca76935c0a3eefe36835
Steve Renner <che1111@hotmail.com>
2020-03-02 16:03:05 -0800

    Login/Registration functional

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['https://pathwaystodestiny.herokuapp.com']
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
-    # 'crispy_forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
